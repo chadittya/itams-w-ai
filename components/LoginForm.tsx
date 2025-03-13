64bf8d65eb6edcc5c9aebbe7bdd7f38849212418
Galih Arizza <galiharizza@test.com>
2025-03-13 23:30:00 +0700

    adding login form with shadcn, with console log on handle submit

---
+++ components/LoginForm.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { Button } from "./ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Input } from "./ui/input";
+
+const loginSchema = z.object({
+  email: z.string().email({
+    message: "Please input valid email address",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 character long!",
+  }),
+});
+
+const LoginForm = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "onBlur",
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof loginSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <>
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Log in</CardTitle>
+          <CardDescription>Log in to your account!</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="foo@bar.com" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input type="password" placeholder="******" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex justify-between">
+                <Button variant="outline">Back</Button>
+                <Button type="submit">Log in</Button>
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default LoginForm;
